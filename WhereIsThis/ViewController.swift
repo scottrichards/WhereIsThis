e5036934f1cbcd7425532dce2b5b8a95564dc8f6
scottrichards <scott.srichards@gmail.com>
2022-06-17 15:52:28 +0200

    Initial Commit

---
+++ WhereIsThis/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WhereIsThis
+//
+//  Created by Scott Richards on 6/17/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
